b0551d6ce106b80223a955df8f337d553c4c2de4
Karim Said <inkriment@outlook.com>
2022-08-10 20:36:50 +0500

    Add project files.

---
+++ ChatSocket .Net Core/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSocket_.Net_Core.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
